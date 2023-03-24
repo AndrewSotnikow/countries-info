45d24138e043a49e080f78057078eb6a1dd96026
AndriiSotnikov <andrii.sotnikov@wtt-solution.com>
2023-03-24 13:17:37 +0100

    add theme reducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,23 @@
-import styled from 'styled-components';
-import {useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { IoMoon, IoMoonOutline } from 'react-icons/io5';
+import styled from 'styled-components'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { IoMoon, IoMoonOutline } from 'react-icons/io5'
 
-import { Container } from './Container';
+import { Container } from './Container'
+import { setTheme } from '../store/theme/theme-actions'
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
   background-color: var(--colors-ui-base);
-`;
+`
 
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 2rem 0;
-`;
+`
 
 const Title = styled(Link).attrs({
   to: '/',
@@ -24,7 +26,7 @@ const Title = styled(Link).attrs({
   font-size: var(--fs-sm);
   text-decoration: none;
   font-weight: var(--fw-bold);
-`;
+`
 
 const ModeSwitcher = styled.div`
   color: var(--colors-text);
@@ -32,30 +34,34 @@ const ModeSwitcher = styled.div`
   cursor: pointer;
   // font-weight: var(--fw-bold);
   text-transform: capitalize;
-`;
+`
 
 export const Header = () => {
-  const theme = 'light';
+  const theme = useSelector((state) => state.theme)
+  const dispatch = useDispatch()
+
+  const toggleTheme = () =>
+    dispatch(setTheme(theme === 'light' ? 'dark' : 'light'))
 
   useEffect(() => {
-    document.body.setAttribute('data-theme', theme);
-  }, [theme]);
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
 
   return (
     <HeaderEl>
       <Container>
         <Wrapper>
           <Title>Where is the world?</Title>
-          <ModeSwitcher>
+          <ModeSwitcher onClick={toggleTheme}>
             {theme === 'light' ? (
-              <IoMoonOutline size="14px" />
+              <IoMoonOutline size='14px' />
             ) : (
-              <IoMoon size="14px" />
+              <IoMoon size='14px' />
             )}{' '}
             <span style={{ marginLeft: '0.75rem' }}>{theme} Theme</span>
           </ModeSwitcher>
         </Wrapper>
       </Container>
     </HeaderEl>
-  );
-};
+  )
+}
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import { rootReducer } from './root-reducer'
-import * as api from '../../config'
+import * as api from '../config'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
